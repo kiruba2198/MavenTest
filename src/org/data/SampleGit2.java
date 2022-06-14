579d2e2748b4c79c5c19a790be3b1f4a9dc4af03
kiruba2198 <kirubachitti21@gmail.com>
2022-06-14 20:05:00 +0100

    this is kamal

--- src/org/data/SampleGit2.java
+++ src/org/data/SampleGit2.java
@@ -2,4 +2,12 @@ package org.data;
 
 public class SampleGit2 {
 
+	private void kamal() {
+	 System.out.println("this is kamal here....");
+
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
